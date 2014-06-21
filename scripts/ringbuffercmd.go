9a5424daf51037fcdb59386f6b0856051b7399e9
Nicholas Weaver <lynxbat@gmail.com>
2014-06-21 12:17:33 -0500

    Added test script

---
+++ scripts/ringbuffercmd.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/lynxbat/ringbuffer"
+	"fmt"	
+)
+
+func main() {
+	size := 100
+	to_write := 1000
+	
+	rb := ringbuffer.NewRingBuffer(size)
+	fmt.Print("**Initial ring buffer**\n")
+	rb.DebugPrint()
+	
+	// Loop 16 times writing a new integer element	
+	for x := 1; x <= to_write; x ++ {
+		// println(x)
+		ie := ringbuffer.NewIntegerElement(x)
+		rb.Write(ie)
+	}
+	
+	fmt.Print("**16 sequence writes**\n")
+	rb.DebugPrint()
+
+	fmt.Print("**Read out all values**\n")
+	for !rb.IsEmpty() {
+		y := rb.Read().GetValue()
+		fmt.Printf("%d ", y)		
+	}
+	fmt.Print("\n\n")
+	
+	fmt.Print("**After read until empty**\n")
+	rb.DebugPrint()
+}
